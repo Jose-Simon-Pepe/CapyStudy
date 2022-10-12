934d789a89e845224dd08d48db768b02243f1224
Jose <jose.s.contacto@gmail.com>
2022-10-12 01:12:52 -0300

    refactored: services utilities has become more abstract and general purpose

--- capycore/src/main/java/capyutilities/ServicesUtilities.java
+++ capycore/src/main/java/capyutilities/ServicesUtilities.java
@@ -6,49 +6,33 @@ import student.Student;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public abstract class ServicesUtilities {
+public abstract class ServicesUtilities<Object> {
 
     protected boolean input_was_completed(String @NotNull ... inputs) {
         return Arrays.stream(inputs).noneMatch(String::isBlank);
     }
 
-    protected boolean input_is_right(@NotNull Student who) {
-        Student toSave;
-        if (passIsRight(who.getPass()) && !stringContainsNumber(who.getName())
-                && !stringContainsNumber(who.getSurname()) && stringStartsWithCapital(who.getName())) {
-            toSave = formatNameAndSurname(who);
-            return stringStartsWithCapital(toSave.getName()) && stringStartsWithCapital(toSave.getSurname());
-        } else {
-            return false;
-        }
+
+    protected String capitalize_first_letter(String word) {
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 
 
-    private Student formatNameAndSurname(Student who) {
-        who.setName(who.getName().substring(0, 1).toUpperCase() + who.getName().substring(1));
-        who.setSurname(who.getSurname().substring(0, 1).toUpperCase() + who.getSurname().substring(1));
-        return who;
-    }
-
-    private boolean passIsRight(String pass) {
-        return stringContainsNumber(pass) && stringContainsCapital(pass) && pass.length() > 8;
-    }
-
-    private boolean stringContainsNumber(@NotNull String string) {
+    protected boolean stringContainsNumber(@NotNull String string) {
         return !string.chars()
                 .filter(Character::isDigit)
                 .mapToObj(c -> Character.toString((char) c))
                 .collect(Collectors.joining()).isBlank();
     }
 
-    private boolean stringContainsCapital(@NotNull String pass) {
+    protected boolean stringContainsCapital(@NotNull String pass) {
         return !pass.chars()
                 .filter(Character::isUpperCase)
                 .mapToObj(c -> Character.toString((char) c))
                 .collect(Collectors.joining()).isBlank();
     }
 
-    private boolean stringStartsWithCapital(@NotNull String string) {
+    protected boolean stringStartsWithCapital(@NotNull String string) {
         return Character.isUpperCase(string.trim().charAt(0));
     }
 
