ce881f316274d2e5d720b8a6580f3cad47c2e67f
Jose <jose.s.contacto@gmail.com>
2022-10-12 13:31:54 -0300

    feature: opening new feature generator

---
+++ capycore/src/test/java/generator/Generator.java
@@ -0,0 +1,12 @@
+package generator;
+
+/**
+ * todo Cómo estudiante, quiero generar repasos espaciados automáticamente
+ * todo Cómo estudiante, quiero generar sesiones de estudio
+ * todo Cómo estudiante, quiero generar fichas de repaso automáticamente a partir de mis notas de estudio
+ */
+public class Generator {
+
+
+
+}
